fae66937886a098c2b3c761278549f694657361e
Ramanpreet12 <raman00613@gmail.com>
2025-12-16 12:48:37 +0530

    add the http api of inngest

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ app.use(cors())
 app.use(clerkMiddleware())
 
 app.get('/', (req,res) => res.send("server is live!"))
+app.use('/api/inngest', serve({client:inngest, functions}))
 app.listen(port,()=>{
     console.log(`Server listening at http://localhost:${port}`);
 })
